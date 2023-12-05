8b6147e8dffb89693b05cea8dc2e062f697c4e29
Ahmad Ali Tresvant <ahmadtresvant@Alis-M1-Pro.local>
2023-12-05 11:32:45 -0500

    created working server and / route

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const client = require('./db/client.js');
+client.connect();
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello Robot</h1>');
+})
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
